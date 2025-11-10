2f8c97dbe3b14c35b74d5d6314ffad68ec624590
stev <stev@stevs-MacBook-Pro.local>
2025-11-10 15:15:42 +0000

    projects update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,8 +31,8 @@ const Navbar = () => {
           
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
-            {/* <li><Link to="/services">services</Link></li> */}
             <li><Link to="/projects">Projects</Link></li>
+            {/* <li><Link to="/services">services</Link></li> */}
             <li><Link to="/contact">Contact</Link></li>
           
         </ul>
--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -1,29 +1,37 @@
-import pro1 from "../assets/project00.png"
-import pro2 from "../assets/todolist.png"
-import pro3 from "../assets/blackjack.png"
-import pro4 from "../assets/stdreg.png"
+import pro1 from "../assets/sidan.png"
+import pro2 from "../assets/project00.png"
+import pro3 from "../assets/todolist.png"
+import pro4 from "../assets/blackjack.png"
+import pro5 from "../assets/stdreg.png"
+
 
 const ProjectCardData = [
     {
         imgsrc: pro1,
+        title: "Construction Management System",
+        text: "A construction project management platform with real-time updates, task tracking, and document management using PHP-Laravel and  Tailwind CSS ",
+        view: "https://www.admin.sidanassociates.com/",
+    },
+    {
+        imgsrc: pro2,
         title: "My Personal Portfolio Website",
         text: "A Personal Portfolio Website that showcase my skills, projects and experience.",
         view: "https://github.com/Stev-ven/Portfolio",
     },
     {
-        imgsrc: pro2,
+        imgsrc: pro3,
         title: "To Do List app",
         text: "A simple To Do List app using React.",
         view: "https://github.com/Stev-ven/react_to-do_list_project",
     },
     {
-        imgsrc: pro3,
+        imgsrc: pro4,
         title: "Black Jack Game",
         text: "A  popular card game played between a player and a dealer. The goal of the game is to get a hand value as close to 21 as possible without exceeding it",
         view: "https://github.com/Stev-ven/BlackJack-app",
     },
     {
-        imgsrc: pro4,
+        imgsrc: pro5,
         title: "Students Registration app",
         text: "Students Registration website using Laravel to improve my backend skills.",
         view: "https://github.com/Stev-ven/students_reg_laravel",
